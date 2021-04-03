8cad802faa2837c70f196d039c612a92573fb411
Pedro Haluch <pedro.haluch@gmail.com>
2021-04-02 23:37:26 -0300

    First Commit

---
+++ app/src/main/java/com/tresmosqueteiros/pi2pdm/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.tresmosqueteiros.pi2pdm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
