3288b8e14b0196d37ddb87449ff47678e3ab7c51
Pedro Haluch <pedro.haluch@gmail.com>
2021-04-10 18:09:41 -0300

    Melhora nos nomes dos ids

--- app/src/main/java/com/tresmosqueteiros/pi2pdm/MainActivity.kt
+++ app/src/main/java/com/tresmosqueteiros/pi2pdm/MainActivity.kt
@@ -2,10 +2,18 @@ package com.tresmosqueteiros.pi2pdm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        txEmailLogin.setOnClickListener(savedInstanceState: Bundle?) {
+            val intencaoDeChamada = Intent(this, SignUpActivity::class.java)
+            startActivity(intencaoDeChamada)
+        }
+
+
     }
 }
